234c557f6ba379ea418703992963533eb006b618
Marius Kohmann <marius.kohmann@gmail.com>
2020-01-28 15:37:29 +0100

    add whitespace to trigger danger agian

--- src/main/kotlin/io/github/mkohm/detekt/hint/rules/InternalClass.kt
+++ src/main/kotlin/io/github/mkohm/detekt/hint/rules/InternalClass.kt
@@ -3,12 +3,12 @@ package io.github.mkohm.detekt.hint.rules
 import io.gitlab.arturbosch.detekt.api.DefaultContext
 
 
-// should not warn
-open class InternalClass: DefaultContext()
-
-// should warn about this
-class AnotherInternalClass : InternalClass()
-
-// should report on this one
-open class A
-class B: A()
+ // should not warn
+ open class InternalClass: DefaultContext()
+//
+ // should warn about this
+ class AnotherInternalClass : InternalClass()
+//
+ // should report on this one
+ open class A
+ class B: A()
