6056ab05f7a504da6389800f52d1dbbbab9fce6a
Marius Kohmann <marius.kohmann@gmail.com>
2020-01-28 11:51:09 +0100

    demo

--- src/main/kotlin/io/github/mkohm/detekt/hint/rules/InternalClass.kt
+++ src/main/kotlin/io/github/mkohm/detekt/hint/rules/InternalClass.kt
@@ -1,4 +1,4 @@
-package io.github.mkohm.detekt.hint
+package io.github.mkohm.detekt.hint.rules
 
 import io.gitlab.arturbosch.detekt.api.DefaultContext
 
@@ -8,3 +8,7 @@ open class InternalClass: DefaultContext()
 
 // should warn about this
 class AnotherInternalClass : InternalClass()
+
+// should report on this one
+open class A
+class B: A()
