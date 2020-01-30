7ad60907c9feb3790b25575e070426209390cd75
Marius Kohmann <marius.kohmann@gmail.com>
2020-01-30 20:27:29 +0100

    use rectangle example as demo

--- src/main/kotlin/io/github/mkohm/detekt/hint/demo/UsingInheritanceInsteadOfComposition.kt
+++ src/main/kotlin/io/github/mkohm/detekt/hint/demo/UsingInheritanceInsteadOfComposition.kt
@@ -1,12 +1,23 @@
 package io.github.mkohm.detekt.hint.demo
 
+open class Rectangle(width: Int, height: Int) {
+    private var width = 0
+    private var height = 0
 
-open class Airplane {
-    fun fly() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    init {
+        this.width = width
+        this.height = height
+    }
+
+    fun setWidth(newWidth: Int) {
+        this.width = newWidth
+    }
+
+    fun setHeight(newHeight: Int) {
+        this.height = newHeight
     }
 }
 
-class AirBus : Airplane() {
+class Square(width: Int, height: Int) : Rectangle(width, height) {
 
 }
