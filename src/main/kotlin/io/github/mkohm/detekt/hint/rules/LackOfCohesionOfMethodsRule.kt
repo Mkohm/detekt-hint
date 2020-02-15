a0dce3dfada5fc52116e83004d58b0dafa2c9b10
Marius Kohmann <marius.kohmann@gmail.com>
2020-02-15 12:38:26 +0100

    wip

--- src/main/kotlin/io/github/mkohm/detekt/hint/rules/LackOfCohesionOfMethodsRule.kt
+++ src/main/kotlin/io/github/mkohm/detekt/hint/rules/LackOfCohesionOfMethodsRule.kt
@@ -5,8 +5,8 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.Debt
 import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
-import io.gitlab.arturbosch.detekt.api.ThresholdRule
 import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtClass
@@ -18,9 +18,8 @@ import org.jetbrains.kotlin.psi.psiUtil.containingClass
 import org.jetbrains.kotlin.psi.psiUtil.isPublic
 import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
 
-class LackOfCohesionOfMethodsRule(config: Config = Config.empty) : ThresholdRule(config, 0) {
-    private var currentPublicFunction: KtNamedFunction? = null
-
+class LackOfCohesionOfMethodsRule(config: Config = Config.empty) : Rule(config) {
+    private lateinit var publicMethods: List<KtNamedFunction>
     override val issue = Issue(
         javaClass.simpleName,
         Severity.CodeSmell,
@@ -33,88 +32,112 @@ class LackOfCohesionOfMethodsRule(config: Config = Config.empty) : ThresholdRule
 
     // todo: consider starting at 1
     private var mf_sum = 0
-    private var fieldCount = 0
-    private var publicMethodsCount = 0
 
     override fun visitClass(klass: KtClass) {
+        if (klass.isInterface() || klass.isEnum() || klass.isInner()) return
+
         mf_sum = 0
-        fieldCount = 0
-        publicMethodsCount = 0
+        publicMethods = getPublicMethods(klass)
+        val publicMethodsCount = publicMethods.size
+        val propertyCount = getPropertyCount(klass)
 
         super.visitClass(klass)
 
-        if (publicMethodsCount == 0 || fieldCount == 0) return
+        if (publicMethodsCount == 0 || propertyCount == 0) return
 
-        val lcom = 1 - (mf_sum.toDouble() / (publicMethodsCount * fieldCount))
+        val lcom = 1 - (mf_sum.toDouble() / (publicMethodsCount * propertyCount))
+        println("Class ${klass.name} has LCOM: $lcom")
         if (lcom > thresholdValue) {
             report(
                 CodeSmell(issue, Entity.from(klass), "${klass.name} have a too high LCOM value: $lcom")
             )
         }
-        //    mf_sum = mf_sum.plus(publicMethodToFieldReferences.toList().sumBy { it.second.size })
-
-        //    println("${klass.name} has LCOM value: $lcom")
     }
 
     override fun visitProperty(property: KtProperty) {
         super.visitProperty(property)
-        fieldCount++
-        val publicMethods =
-            property.containingClass()?.collectDescendantsOfType<KtNamedFunction> { it.isPublic } ?: arrayListOf()
-        publicMethodsCount = publicMethods.size
+        if (property.containingClass() == null) return
+
+
 
         for (publicMethod in publicMethods) {
-            val referenceExpression = publicMethod.bodyExpression?.collectDescendantsOfType<KtReferenceExpression> {
-                it.text == property.name
-            } ?: arrayListOf()
+            val referenceExpression = getReferencesOfProperty(publicMethod, property)
 
             if (referenceExpression.isNotEmpty()) {
                 mf_sum++
-                return
+                continue
             }
 
-            if (referenceExpression.isEmpty()) {
-                // look in all local methods that is called from this public function, recursively
-                val allCalleesFromThisPublicMethod = getCallees(publicMethod)
-                val a = 1
+            // Fetch all methods that is called from this public function, recursively.
+            val allCalleesFromThisPublicMethod = getCallees(publicMethod)
 
-                // val allPrivateMethodsOfThisPublicMethod = allPrivateMethods.filter { it.name.toString() == publicMethod.findDescendantOfType<KtCallExpression>(it.name) }
+            // We look for references of the property, and as soon as we find it, we increase mf_sum and break so that we can look for references in other public methods.
+            for (privateMethod in allCalleesFromThisPublicMethod) {
 
+                // Get references in this private function
+                val references = getReferencesOfProperty(privateMethod, property)
 
-                 for (privateMethod in allCalleesFromThisPublicMethod) {
-                     val referenceExpression = privateMethod.bodyExpression?.collectDescendantsOfType<KtReferenceExpression> {
-                         it.text == property.name
-                     } ?: arrayListOf()
-
-                     if (referenceExpression.isNotEmpty()) {
-                         mf_sum ++
-                         return
-                     }
-                 }
+                if (references.isNotEmpty()) {
+                    mf_sum++
+                    break
+                }
             }
         }
-
-        // for each public method
-        // check if property is referenced in the public method
-        // if not referenced - check private methods
     }
 
-    private fun getCallees(publicMethod: KtNamedFunction): List<KtNamedFunction> {
-        val doesNotHaveDecendants =
-            ArrayList(publicMethod.collectDescendantsOfType<KtCallExpression>().map { it.getResolvedCall(bindingContext)?.resultingDescriptor?.findPsi() as KtNamedFunction }).size == 0
+    private fun getPublicMethods(klass: KtClass): List<KtNamedFunction> {
+        return klass.collectDescendantsOfType<KtNamedFunction> { it.isPublic }
+    }
 
-        if (doesNotHaveDecendants) {
-            return arrayListOf<KtNamedFunction>()
+    private fun getPropertyCount(klass: KtClass): Int {
+        return klass.getProperties().size
+    }
+
+    private fun getReferencesOfProperty(
+        privateMethod: KtNamedFunction,
+        property: KtProperty
+    ): List<KtReferenceExpression> {
+        return privateMethod.bodyExpression?.collectDescendantsOfType { reference ->
+            isReferenceOfPropertyClass(reference, property) && reference.text == property.name
+        } ?: arrayListOf()
+    }
+
+    /**
+     * We need to ensure that the found reference not only have the same name as the property, but if it is a member of the correct class as well.
+     */
+    private fun isReferenceOfPropertyClass(
+        reference: KtReferenceExpression,
+        property: KtProperty
+    ) =
+        reference.getResolvedCall(bindingContext)?.resultingDescriptor?.containingDeclaration?.name.toString() == property.containingClass()?.name
+
+    private fun getCallees(publicMethod: KtNamedFunction, foundCallees: List<KtNamedFunction>): List<KtNamedFunction> {
+
+        // If we have any call expressions inside the parent function with the same name as the parent function - There are some possibilities;
+        // 1. calling another function with the same name, but with different signature
+        // 2. calling the supermethod
+        // 3. calles itself - we have recursion.
+        // As a quick fix for now we filter out the call expressions with the same name as the parent function. This will make us loose references to properties that are in functions with the same name.
+
+        val callExpressions =
+            publicMethod.collectDescendantsOfType<KtCallExpression>().filter { it.firstChild.text != publicMethod.name }
+        if (callExpressions.isEmpty()) {
+            return arrayListOf()
         }
-        val callees = ArrayList(publicMethod.collectDescendantsOfType<KtCallExpression>().map {
-            it.getResolvedCall(bindingContext)?.resultingDescriptor?.findPsi() as KtNamedFunction
-        })
+
+        val callees = callExpressions.mapNotNull {
+            try {
+                it.getResolvedCall(bindingContext)?.resultingDescriptor?.findPsi() as KtNamedFunction
+            } catch (e: ClassCastException) {
+                null
+            }
+        }
+
 
         for (callee in callees) {
-            return callees + getCallees(callee)
+            callees + getCallees(callee, callees)
         }
 
-        return listOf<KtNamedFunction>()
+        return callees
     }
-
 }
--- src/test/kotlin/io/github/mkohm/detekt/hint/LackOfCohesionMethodsRuleSpec.kt
+++ src/test/kotlin/io/github/mkohm/detekt/hint/LackOfCohesionMethodsRuleSpec.kt
@@ -17,6 +17,137 @@ class LackOfCohesionMethodsRuleSpec : Spek({
         factory = { KtTestCompiler.createEnvironment() },
         destructor = { it.dispose() }
     )
+    describe("Using properties outside of class should not count as a property.") {
+        it("Should give correct LCOM value") {
+
+            // language=kotlin
+            val code = """
+                var a = 1
+                
+                class Foo {
+                    private var number = 0
+                    
+                    fun bar() {
+                        Baz().number++
+                    }
+                }
+                
+                class Baz {
+                    var number = 0
+                }
+                """.trimIndent()
+
+            val findings = LackOfCohesionOfMethodsRule(testConfig).compileAndLintWithContext(wrapper.env, code)
+
+            val f = 1
+            val m = 1
+            val mf = 0
+            val lcom = 1 - (mf.toDouble() / (m * f))
+
+            assertThat(findings.first().message).contains("Foo have a too high LCOM value: $lcom")
+        }
+    }
+
+    describe("Referencing a property with the same name in another class should not count as a reference.") {
+        it("Should give correct LCOM value") {
+
+            // language=kotlin
+            val code = """
+                class Foo {
+                    private var number = 0
+                    
+                    fun bar() {
+                        Baz().number++
+                    }
+                }
+                
+                class Baz {
+                    var number = 0
+                }
+                """.trimIndent()
+
+            val findings = LackOfCohesionOfMethodsRule(testConfig).compileAndLintWithContext(wrapper.env, code)
+
+            val f = 1
+            val m = 1
+            val mf = 0
+            val lcom = 1 - (mf.toDouble() / (m * f))
+
+            assertThat(findings.first().message).contains("Foo have a too high LCOM value: $lcom")
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    describe("Simple class") {
+        it("Should give correct LCOM value") {
+
+            // language=kotlin
+            val code = """
+                class Foo {
+
+                    private var number = 0
+                    private var number2 = 0
+                    
+                    fun bar() {
+                        bar("string")
+                    }
+                    
+                    fun bar(string: String) {
+                        number++
+                    }
+                }
+                """.trimIndent()
+
+            val findings = LackOfCohesionOfMethodsRule(testConfig).compileAndLintWithContext(wrapper.env, code)
+
+            val f = 2
+            val m = 2
+            val mf = 1
+            val lcom = 1 - (mf.toDouble() / (m * f))
+
+            assertThat(findings.first().message).contains(lcom.toString())
+        }
+    }
+
+    describe("Simple class") {
+        it("Should give correct LCOM value") {
+
+            // language=kotlin
+            val code = """
+                    class Bar { 
+                        private var number = 0
+                        private var number2 = 0
+
+                        fun add()  {
+                            number++
+                            number2++
+                        }
+                                            
+                        fun subtract() {
+                            number--
+                        }
+                    }
+                """.trimIndent()
+
+            val findings = LackOfCohesionOfMethodsRule(testConfig).compileAndLintWithContext(wrapper.env, code)
+
+            val f = 2
+            val m = 2
+            val mf = 3
+            val lcom = 1 - (mf.toDouble() / (m * f))
+
+            assertThat(findings.first().message).contains(lcom.toString())
+        }
+    }
 
     describe("Class with private methods") {
         it("Should give correct LCOM value") {
@@ -103,36 +234,6 @@ class LackOfCohesionMethodsRuleSpec : Spek({
         }
     }
 
-    describe("Simple class") {
-        it("Should give correct LCOM value") {
-
-            // language=kotlin
-            val code = """
-                    class Bar { 
-                        private var number = 0
-                        private var number2 = 0
-
-                        fun add()  {
-                            number++
-                            number2++
-                        }
-                                            
-                        fun subtract() {
-                            number--
-                        }
-                    }
-                """.trimIndent()
-
-            val findings = LackOfCohesionOfMethodsRule(testConfig).compileAndLintWithContext(wrapper.env, code)
-
-            val f = 2
-            val m = 2
-            val mf = 3
-            val lcom = 1 - (mf.toDouble() / (m * f))
-
-            assertThat(findings.first().message).contains(lcom.toString())
-        }
-    }
 
     describe("Full cohesion of all the method and properties") {
         it("Should give LCOM 0") {
@@ -229,7 +330,7 @@ class LackOfCohesionMethodsRuleSpec : Spek({
             val findings = LackOfCohesionOfMethodsRule(testConfig).compileAndLintWithContext(wrapper.env, code)
             assertThat(findings.first().message).contains(lcom.toString())
 
-            val f2= 2
+            val f2 = 2
             val m2 = 2
             val mf2 = 3
             val lcom2 = 1 - (mf2.toDouble() / (m2 * f2))
